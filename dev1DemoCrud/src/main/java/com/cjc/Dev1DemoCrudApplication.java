3612f91ae39623d404b85dfb9efdf48c1a65bd4a
swaranjalisorate <swaranjalisorate@gmail.com>
2023-08-07 11:21:23 +0530

    dev1

---
+++ dev1DemoCrud/src/main/java/com/cjc/Dev1DemoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev1DemoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dev1DemoCrudApplication.class, args);
+	}
+
+}
